318f767c023a2816e88e5b8aae30a1f3d3913f66
bhavyakc09 <124873407+bhavyakc09@users.noreply.github.com>
2023-02-17 13:20:48 +0530

    Update read_excel.py

--- read_excel.py
+++ read_excel.py
@@ -6,9 +6,9 @@ if len(sys.argv) < 2:
     print("Please provide the path to the Excel file as a command line argument.")
     sys.exit(1)
 
-excel_file_path = sys.argv[1]
+excel_file_path = os.environ['EXCEL_FILE_PATH']
 
-workbook = load_workbook(filename=os.environ['D:\\demo\\data.xlsx'])
+workbook = load_workbook(filename=excel_file_path)
 worksheet = workbook.active
 
 for row in worksheet.iter_rows(min_row=2, values_only=True):
