4978ea4b6440d8f587cc4e6195fe7b3dc7f60b99
bhavyakc09 <124873407+bhavyakc09@users.noreply.github.com>
2023-02-17 14:04:55 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -3,6 +3,6 @@ print(os.getcwd())
 import os
 from openpyxl import load_workbook
 
-excel_file_path = os.environ['EXCEL_FILE_PATH']
+excel_file_path = os.environ['D:\\demo\\data.xlsx']
 
-workbook = load_workbook(filename=excel_file_path)
+workbook = load_workbook(filename=D:\\demo\\data.xlsx)
