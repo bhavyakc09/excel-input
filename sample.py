1fd3751f269c3b280cfb6474effba3ec60317d00
bhavyakc09 <124873407+bhavyakc09@users.noreply.github.com>
2023-02-17 15:35:18 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -4,8 +4,3 @@ from openpyxl import load_workbook
 excel_file_path = sys.argv[1]
 
 workbook = load_workbook(filename=excel_file_path)
-
-for i in os.listdir(os.getcwd()):
-    if os.path.isfile(i):
-        if i.endswith(".xls") or i.endswith(".xlsx"):
-            wb = openpyxl.load_workbook(os.path.join(os.getcwd(), i))
